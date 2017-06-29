3c9430b06d5d7c515c3de5b466f187760cbb0878
Sacha Morard <smorard@idobs.net>
2017-06-29 09:31:21 +0200

    travis

--- createdb.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker exec -it cassandra cqlsh -e 'DROP KEYSPACE IF EXISTS testphalcon'
-docker exec -it cassandra cqlsh -e "CREATE KEYSPACE testphalcon WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }"
