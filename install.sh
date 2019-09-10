367b25f22495b90f8146e9034a795be0a482d468
Benjamin Leseur <bleseur@idobs.net>
2019-09-10 17:21:18 +0200

    Build for PHP 7.2

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ docker network create --driver=bridge --subnet=172.30.0.0/16 my-net
 docker pull cassandra:3.10
 docker run -d -p 0.0.0.0:9160:9160 -p 0.0.0.0:9042:9042 --network=my-net --ip=172.30.100.104 --rm --name cassandra --hostname cassandra cassandra
 #docker build docker/php7.1 -t sachamorard/php-phalcon-cassandra
-docker pull sachamorard/php-phalcon-cassandra
+docker build docker/php7.2 -t sachamorard/php-phalcon-cassandra
+#docker pull sachamorard/php-phalcon-cassandra
 docker run -d --name php-phalcon-cassandra --network=my-net --rm -v `pwd`:/home/www -w /home/www --add-host=cassandra:172.30.100.104 sachamorard/php-phalcon-cassandra
 docker exec -it php-phalcon-cassandra composer install --prefer-source --no-interaction
 sleep 20
--- tests/unit/PhalconCest.php
+++ tests/unit/PhalconCest.php
@@ -7,7 +7,7 @@ class PhalconCest
 {
     public function version(\UnitTester $I)
     {
-        $I->assertEquals(\Phalcon\Version::get(), '3.3.1');
+        $I->assertEquals(\Phalcon\Version::get(), '3.4.4');
     }
 }
 
